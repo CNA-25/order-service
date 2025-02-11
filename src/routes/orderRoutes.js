75276249d988edb9850ecff0cd4ddfc1b7a6ab1b
hooliganfreak <ivarsemi@arcada.fi>
2025-02-11 15:03:52 +0200

    testin sendOrder

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -276,15 +276,13 @@ router.post("/orders", getCartData, checkInventory, async (req, res) => {
 
 
 
-    /* Send the new order to invoice and email
+    // Send the new order to invoice and email
 
     const orderSent = await sendOrder(newOrder);
     console.log(orderSent);
     if (!orderSent) {
       throw new Error("Kunde inte skicka beställningen vidare.");
     }
-
-    */
 
     // Returnera success
     res.status(201).json({
